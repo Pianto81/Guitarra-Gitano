47a21248d951cb6596e4f6c9c772cd644f0585e7
Pianto81 <90965863+Pianto81@users.noreply.github.com>
2021-09-18 15:30:29 +0200

    first commit

---
+++ config/bot.js
@@ -0,0 +1,16 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: '<:Fuckyou:822821006468186122>',
+        queue: ':bar_chart:',
+        music: '<:coinc:829412222341087262>',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        prefix: '&',
+        activity: 'fortnite = bien',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
