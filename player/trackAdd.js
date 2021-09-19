787732456b8a76df94212820bcc769f1a10dd5c9
Pianto81 <90965863+Pianto81@users.noreply.github.com>
2021-09-19 13:29:34 +0200

    Update trackAdd.js

--- player/trackAdd.js
+++ player/trackAdd.js
@@ -1,20 +1,3 @@
 module.exports = (client, message, queue, track) => {
-    message.channel.send({
-    embed: {
-        color: '#4a6c98',
-        author: { name: client.emotes.music+'playing ' + track.title },
-        footer: { text: 'This bot uses a Github project made by Zerio (ZerioDev/Music-bot)' },
-        fields: [
-            { name: 'Channel', value: track.author, inline: true },
-            { name: 'Requested by', value: track.requestedBy.username, inline: true },
-            { name: 'From playlist', value: track.fromPlaylist ? 'Yes' : 'No', inline: true },
-
-            { name: 'Duration', value: track.duration, inline: true },
-            { name: 'Filters activated', value: filters.length + '/' + client.filters.length, inline: true },
-
-        ],
-        thumbnail: { url: track.thumbnail },
-        timestamp: new Date(),
-    },
-});
+    message.channel.send(`${client.emotes.music} - ${track.title} has been added to the queue !`);
 };
